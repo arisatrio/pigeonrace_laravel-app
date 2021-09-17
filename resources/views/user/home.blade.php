16948c206d472884be1289d5430363f300eef279
ariSatriooo <arysatrio097@gmail.com>
2021-09-17 08:55:58 +0700

    fix

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -43,7 +43,7 @@
                     </div>
                 </div>
                 <div class="article-details">
-                    <small><b> {{ $item->tgl_race->diffForHumans() }} </b></small>
+                    <small><b> {{ $item->tgl_race->format('d F Y') }} </b></small>
                     <p>{{ $item->deskripsi }}</p>
                     <div class="article-cta">
                         <a href="{{ route('user.race-mode', $item->id) }}" class="btn btn-lg btn-primary">Buka</a>
