aeb301eab76d444aaec06dcaf8e15cb865c3a9e2
ariSatriooo <arysatrio097@gmail.com>
2021-09-27 11:17:14 +0700

    fix hasil race

--- resources/views/user/riwayat-pos-rank.blade.php
+++ resources/views/user/riwayat-pos-rank.blade.php
@@ -48,19 +48,19 @@
                     <table class="table table-striped display-nowrap" cellspacing="0" width="100%" id="table-1">
                         <thead>
                             <tr class="text-center bg-dark">
-                                <th colspan="9" class="text-white">Pos {{ $pos->no_pos }} - {{ $pos->city }} - {{$kelas->nama_kelas}}</th>
+                                <th colspan="10" class="text-white">Pos {{ $pos->no_pos }} - {{ $pos->city }} - {{$kelas->nama_kelas}}</th>
                             </tr>
                             <tr class="bg-info">
                                 <th class="bg-danger text-white all" style="width: 2%;">Rank</th>
                                 <th class="text-white all" style="width: 30%;">Burung</th>
                                 <th class="text-white none">Jarak</th>
-                                <th class="text-white none">Tanggal</th>
+                                <th class="text-white none">Clock</th>
                                 <th class="text-white none">H</th>
-                                <th class="text-white none">Jam</th>
+                                {{-- <th class="text-white none">Jam</th> --}}
                                 <th class="text-white none">Waktu Terbang</th>
                                 <th class="text-white none">Kecepatan</th>
-                                <th class="text-white none" style="width: 5%;">No Stiker</th>
-                                <th class="text-white none" style="width: 5%;">Status</th>
+                                <th class="text-white none">No Stiker</th>
+                                <th class="text-white none">Status</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -70,14 +70,20 @@
                                 <td @if ($item->user->id === auth()->user()->id)  class="bg-primary text-white" @endif>
                                     {{ Helper::birdName($item, $item->user->name) }}
                                 </td>
-                                <td>{{ $item->clock->distance }} KM</td>
-                                <td>{{ $item->clock->arrival_clock->format('d/m/Y') }}</td>
-                                <td>+{{ $item->clock->arrival_day }}</td>
-                                <td>{{ $item->clock->arrival_clock->format('H:i:s') }}</td>
-                                <td>{{ $item->clock->flying_time }}</td>
-                                <td><b>{{ $item->clock->velocity }} M/Menit</b></td>
-                                <td style="width: 5%;">{{$item->clock->no_stiker}}</td>
-                                <td style="width: 5%;">{{$item->clock->status}}</td>
+                                <td><h6>{{ $item->clock->distance }} KM</h6></td>
+                                <td><h6>{{ $item->clock->arrival_clock->format('d/m/Y') }} {{ $item->clock->arrival_clock->format('H:i:s') }}</h6></td>
+                                <td><h6>+{{ $item->clock->arrival_day }}</h6></td>
+                                {{-- <td>{{ $item->clock->arrival_clock->format('H:i:s') }}</td> --}}
+                                <td><h6>{{ $item->clock->flying_time }}</h6></td>
+                                <td><h6>{{ $item->clock->velocity }} M/Menit</h6></td>
+                                <td><h6>{{$item->clock->no_stiker}}</h6></td>
+                                <td><h6>
+                                    @if ($item->clock->status === 'SAH')
+                                        <span class="badge badge-success">{{$item->clock->status}}</span>
+                                    @else
+                                        <span class="badge badge-warning">DIVALIDASI</span>
+                                    @endif
+                                </h6></td>
                             </tr>
                             @endforeach
                         </tbody>
